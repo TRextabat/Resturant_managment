e9862fcd1715bab72f92395b9e07e90eaaaea3dd
Trex <amiramiritabat01@gmail.com>
2025-05-26 21:53:19 +0300

    import all models

--- src/db/alembic/env.py
+++ src/db/alembic/env.py
@@ -9,7 +9,10 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 from alembic import context
 from src.db.database import Base, DATABASE_URL
 from src.user.models import User, Admin, Waiter, Customer, KitchenStaff
-
+from src.order.models import Order, OrderItem
+from src.menu.models import  MenuItem
+from src.table.models import RestaurantTable
+from src.db.models import waiter_table_link
 from src.core.settings import settings
 
 
